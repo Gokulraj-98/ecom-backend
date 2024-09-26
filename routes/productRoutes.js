7c302ba739f1fd47c567de623c8206a98efae659
gokulraj-98 <gokulraj8248@gmail.com>
2024-09-26 11:03:57 +0530

    modified the import

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const Product = require('../models/Product'); // Your Product model
+const Product = require('../models/product');
 const router = express.Router();
 
 // @route   GET /api/products
 // @desc    Get all products
 router.get('/products', async (req, res) => {
     try {
-        const products = await Product.find(); // Fetch all products
+        const products = await Product.find();
         res.send(products);
         console.log(products.title)
     } catch (error) {
